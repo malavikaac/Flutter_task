35a86aa5e207aac4fb78f8d78bcf195099ea7385
malavikaac <acmalavika@gmail.com>
2024-01-03 16:46:44 +0530

    chore

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,6 +6,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      
+    );
   }
 }
