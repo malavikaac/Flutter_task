f1b75e7c2b67c8e38938651f42ebd0f8b316c998
malavikaac <acmalavika@gmail.com>
2024-02-01 17:43:26 +0530

    refactor

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_task/popup_extension/custom_popup.dart';
 
@@ -13,14 +12,14 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-      title: const Text("Profile"),
-      centerTitle: true,
-      backgroundColor: const Color.fromARGB(255, 252, 226, 193),
-     ),
+      appBar: AppBar(
+        title: const Text("Profile"),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 252, 226, 193),
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Expanded(
             child: Container(),
@@ -33,51 +32,47 @@ class _MyHomePageState extends State<MyHomePage> {
               Padding(
                 padding: const EdgeInsets.all(40.0),
                 child: MyCustomPopup(
-                 barrierColor: Colors.transparent,
+                  barrierColor: Colors.transparent,
                   backgroundColor: Colors.white,
-                  arrowColor:  const Color.fromARGB(255, 255, 255, 255),
-                  content: Stack(
-                    children: [
+                  arrowColor: const Color.fromARGB(255, 255, 255, 255),
+                  content: Stack(children: [
                     Container(
-                                  height: 250,
-                                  width: 300,
-                                  decoration: BoxDecoration(color: const Color.fromARGB(255, 255, 255, 255),
-                                  borderRadius:BorderRadius.circular(15   ) ,
-                                  border: Border.all(width: 0.6 ,color:Colors.grey)),
-                                  child: Column(
-                                    children: [
-                                      Container(
-                                        height: 100,
-                                        width: 300,
-                                        decoration: const BoxDecoration(color: Color(0xffD7E8D6),
-                                         borderRadius:BorderRadius.only(topLeft: Radius.circular(15),topRight:  Radius.circular(15))
-                                        ),
-                                      ),
-                                      const SizedBox(height: 30),
-                                      const Text("Sundar"),
-                                      const Text("sundar@gmail.com"),
-                                    ],
-                                  ),
-                                ),
-                                const Positioned(
-                                        top: 50,
-                                        right: 110,
-                                        child: CircleAvatar(
-                                            maxRadius: 40,
-                                            backgroundColor: Color.fromARGB(255, 173, 173, 173),
-                                          ),
-                                      ),
-                                      
-                    ]
-                  ),
+                      height: 250,
+                      width: 300,
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                          borderRadius: BorderRadius.circular(15),
+                          border: Border.all(width: 0.6, color: Colors.grey)),
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 100,
+                            width: 300,
+                            decoration: const BoxDecoration(
+                                color: Color(0xffD7E8D6),
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(15),
+                                    topRight: Radius.circular(15))),
+                          ),
+                          const SizedBox(height: 30),
+                          const Text("Sundar"),
+                          const Text("sundar@gmail.com"),
+                        ],
+                      ),
+                    ),
+                    const Positioned(
+                      top: 50,
+                      right: 110,
+                      child: CircleAvatar(
+                        maxRadius: 40,
+                        backgroundColor: Color.fromARGB(255, 173, 173, 173),
+                      ),
+                    ),
+                  ]),
                   child: const Icon(Icons.help, color: Colors.grey),
                 ),
               ),
               const SizedBox(height: 20),
-              
-           
-              
-             
             ],
           ),
         ],
@@ -85,5 +80,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-
